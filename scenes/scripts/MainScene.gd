41c8079011182922172ae8bc9e2561786b0ad013
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-11-26 15:14:55 +0000

    making camera and fpv exclusive

--- scenes/scripts/MainScene.gd
+++ scenes/scripts/MainScene.gd
@@ -4,5 +4,7 @@ extends Spatial
 func _on_Player_view_switched(view):
 	if view == Enums.VIEW_MODES.TPV:
 		$UI/OverlayCanvas/DebugUI.hide()
+		$UI/OverlayCanvas/DriverDash.hide()
 	else:
 		$UI/OverlayCanvas/DebugUI.show()
+		$UI/OverlayCanvas/DriverDash.show()
--- scenes/scripts/Player.gd
+++ scenes/scripts/Player.gd
@@ -12,6 +12,7 @@ onready var view_mode = Enums.VIEW_MODES.FPV
 # y is up and down
 var last_burn_rotation: float = 0.0
 
+var camera_rotation_speed:float = 1.0
 onready var tp_camera: Camera = $CameraPivot/TPCamera
 onready var fp_camera: Camera = $FPCamera
 onready var starting_pos: Vector3 = translation
@@ -21,9 +22,35 @@ onready var starting_pos: Vector3 = translation
 func _process(delta):
 	if Input.is_action_just_released("view_change"):
 		toggle_view()
+	
+	if view_mode == Enums.VIEW_MODES.TPV:
+		check_tpv_camera_input()
 
 func _physics_process(delta):
+	var torque = Vector3.ZERO
+	var burn_dir = null
+	var torque_type = null
+	var dir_type = null
 	
+	# if in FPV you can drive
+	if view_mode == Enums.VIEW_MODES.FPV:
+		var inputs =  get_driving_inputs()
+		torque = inputs[0]
+		torque_type = inputs[1]
+		burn_dir = inputs[2]
+		dir_type = inputs[3]
+	
+	# this is so upper view camera wont rotate
+	if torque != Vector3.ZERO:
+		$CameraPivot.rotation = -rotation
+		add_torque(torque * rotation_speed * delta)
+		emit_signal("torque_update", torque_type)
+	
+	if burn_dir != null:
+		add_central_force(burn_dir * boost_force * delta)
+		emit_signal("direction_update", dir_type)
+
+func get_driving_inputs() -> Array:
 	var torque = Vector3.ZERO
 	var torque_type = Enums.DIRECTIONS.NONE
 	
@@ -34,15 +61,10 @@ func _physics_process(delta):
 		torque.y = 1
 		torque_type = Enums.DIRECTIONS.LEFT
 	
-	# this is so upper view camera wont rotate
-	$CameraPivot.rotation = -rotation
-	if torque != Vector3.ZERO:
-		add_torque(torque * rotation_speed * delta)
-		emit_signal("torque_update", torque_type)
 	
 	var is_toggled = Input.is_action_pressed("toggle")
 	var force = Vector3.ZERO
-	var burn_dir = 0
+	var burn_dir = null
 	var dir_type = null
 	
 	if Input.is_action_pressed("burn"):
@@ -55,16 +77,39 @@ func _physics_process(delta):
 		dir_type = Enums.DIRECTIONS.BACK
 		
 	
-	
-	if burn_dir != 0:
+	if burn_dir != null:
 		var x_dir = sin(last_burn_rotation) if !is_toggled else 0
 		var z_dir = cos(last_burn_rotation) if !is_toggled else 0
 		var y_dir = 0 if !is_toggled else -1
 		dir_type = dir_type if !is_toggled else Enums.DIRECTIONS.UP if burn_dir == -1 else Enums.DIRECTIONS.DOWN
+		burn_dir = (Vector3(x_dir, y_dir, z_dir) * burn_dir).normalized()
 		
-		force = (burn_dir * Vector3(x_dir, y_dir, z_dir)).normalized()
-		add_central_force(force * boost_force * delta)
-		emit_signal("direction_update", dir_type)
+		
+	return [torque, torque_type, burn_dir, dir_type]
+
+func check_tpv_camera_input():
+	# maybe those need to rotate with delta on an axis only maybe
+	# also might need to rotate camera too as it seems to be upside down
+	if Input.is_action_just_released("right"):
+		$CameraPivot.rotation_degrees.x = 0
+		$CameraPivot.rotation_degrees.z = -90
+		$CameraPivot/TPCamera.translation.y = 10
+	elif Input.is_action_just_released("left"):
+		$CameraPivot.rotation_degrees.x = 0
+		$CameraPivot.rotation_degrees.z = 90
+		$CameraPivot/TPCamera.translation.y = 10
+	elif Input.is_action_just_released("burn"):
+		$CameraPivot.rotation_degrees.z = 0
+		$CameraPivot.rotation_degrees.x = 90
+		$CameraPivot/TPCamera.translation.y = 10
+	elif Input.is_action_just_released("counter_burn"):
+		$CameraPivot.rotation_degrees.x = -90
+		$CameraPivot.rotation_degrees.z = 0
+		$CameraPivot/TPCamera.translation.y = 10
+	elif Input.is_action_just_released("toggle"):
+		$CameraPivot.rotation_degrees.x = 0
+		$CameraPivot.rotation_degrees.z = 0
+		$CameraPivot/TPCamera.translation.y = 100
 
 func toggle_view():
 	if view_mode == Enums.VIEW_MODES.FPV:
