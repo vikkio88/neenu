b8d9842d5094eb014aa0edf901a9a5fd1799403f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-12-27 17:29:41 +0000

    improved driver dash interface

--- ui/scripts/DriverDash.gd
+++ ui/scripts/DriverDash.gd
@@ -1,25 +1,29 @@
 extends Control
 
+var directions_pushed = Array();
+
 func report_direction(direction, active):
 	var green = Color(0,.7,0)
-	reset_directions()
-	match direction:
-		Enums.DIRECTIONS.FORWARD:
-			$Forward.color = green
-		Enums.DIRECTIONS.BACK:
-			$Back.color = green
-		Enums.DIRECTIONS.UP:
-			$Up.color = green
-		Enums.DIRECTIONS.DOWN:
-			$Down.color = green
-		Enums.DIRECTIONS.LEFT:
-			$Left.color = green
-		Enums.DIRECTIONS.RIGHT:
-			$Right.color = green
-		_:
-			pass
+	var node = null
 	
-	$DirectionsResetClock.start()
+	if direction == Enums.DIRECTIONS.FORWARD:
+		node = $Forward
+	if direction == Enums.DIRECTIONS.BACK:
+		node = $Back
+	if direction == Enums.DIRECTIONS.UP:
+		node = $Up
+	if direction == Enums.DIRECTIONS.DOWN:
+		node = $Down
+	if direction == Enums.DIRECTIONS.LEFT:
+		node = $Left
+	if direction == Enums.DIRECTIONS.RIGHT:
+		node = $Right
+	
+	if node !=  null:
+		node.color = green
+		if directions_pushed.back() != node:
+			directions_pushed.push_back(node)
+	
 			
 func reset_directions():
 	var base_color = Color(1,1,1)
@@ -46,7 +50,9 @@ func reset_message():
 	$MessageLbl.text = ''
 
 func _on_DirectionsResetClock_timeout():
-	reset_directions()
+	if directions_pushed.size() > 0:
+		var indicator: ColorRect = directions_pushed.pop_front()
+		indicator.color = Color(1,1,1)
 
 
 func _on_MessageResetClock_timeout():
